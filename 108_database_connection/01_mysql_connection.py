751e8f0ae24ab7b5eb59bec0772a8d7f33216f15
sukantahui <huisukanta77@gmail.com>
2023-10-07 20:13:01 +0530

    update 34

--- 108_database_connection/01_mysql_connection.py
+++ 108_database_connection/01_mysql_connection.py
@@ -1,9 +1,26 @@
-import mysql. connector as m
-mycon=m.connect(host='localhost',user='root',password='sukantahui',database='ses_gold')
-if mycon.is_connected():
-    print("Database is connected")
+import mysql.connector as m
+try:
+    mycon=m.connect(
+        host='localhost',
+        user='root',
+        password='sukantahui',
+        database='office_db')
+    if mycon.is_connected():
+        print("MYSQL is connected")
+except:
+    print("Unable to connect")    
+    quit()
 mycur=mycon.cursor()
-mycur.execute('select * from job_master')
-myData=mycur.fetchall()
-for row in myData:
+sql="""CREATE TABLE states (
+    id bigint(20)  PRIMARY KEY AUTO_INCREMENT,
+    state_name varchar(255) NOT NULL,
+    state_code int(11) NOT NULL)"""
+mycur.execute(sql)
+mycur.execute("show tables")
+# # myData=mycur.fetchall()
+# # print(myData)
+# # for row in myData:
+# #     print(row[0])
+print("Tables are: ")
+for row in mycur:
     print(row[0])
